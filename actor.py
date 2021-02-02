f09011fdc1be6d468919d858f8cf04967e0ccb56
Emil Nylen-Forthun <emil.nylen.forthun@gmail.com>
2021-02-02 16:42:46 +0100

    Worked with main and cleaned up some code

--- actor.py
+++ actor.py
@@ -1,53 +1,62 @@
 from collections import defaultdict
-class Actor():
+import random
 
-  def __init__(self, learning_rate, discount_factor, eli_decay, epsilon=None):
-    self.epsilon = epsilon if epsilon else 0
-    self.policy_dict = defaultdict(lambda:0)
-    self.eli_dict = defaultdict(lambda:0)
-    self.discount_factor = discount_factor
-    self.eli_decay = eli_decay
-    self.learning_rate = learning_rate
-  
-  #Updates policy using: current_policy + learning_rate*td_err*eli_dict[state,action]
-  def update_policy_dict(self, state, action, td_err):
-    self.policy_dict[(str(state), str(action))] += self.learning_rate*td_err*self.eli_dict[(str(state), str(action))]
-  
-  #Updates eligibility using: discount_factor*eli_decay*eli_dict[state,action]
-  def update_eli_dict(self, state,action, i):
-    if i == 0:
-      self.eli_dict[(str(state), str(action))] = 1
-    else:
-      self.eli_dict[(str(state), str(action))] = self.discount_factor*self.eli_decay*eli_dict[(str(state), str(action))]
-  
-  def get_policy(self, state, action):
-    if (str(state), str(action)) in self.policy_dict.keys():
-      return self.policy_dict[(str(state), str(action))]
-    else:
-      return 0
-  
-  def reset_eli_dict(self):
-    self.eli_dict = {}
-  
-  def get_action(self, state, legal_actions):
-    return max(legal_actions, key=lambda action: self.get_policy(state, action))
-  
-actor = Actor(0.2, 0.2,0.2)
 
-actor.update_eli_dict([[1, 1, 1, 1], [1, 1, 1, 1], [1, 0, 1, 1], [1, 1, 1, 1]], [(2, 3), (2, 1)], 0)
+class Actor:
 
-actor.update_policy_dict([[1, 1, 1, 1], [1, 1, 1, 1], [1, 0, 1, 1], [1, 1, 1, 1]], [(2, 3), (2, 1)], 0.3)
-print(actor.policy_dict)
-print(actor.get_policy([[1, 1, 1, 1], [1, 1, 1, 1], [1, 1, 1, 1], [1, 1, 1, 1]], [(2, 3), (2, 1)]))
-print(actor.get_action([[1, 1, 1, 1], [1, 1, 1, 1], [1, 0, 1, 1], [1, 1, 1, 1]], [[(2, 1), (1, 1)], [(2, 3), (2, 1)]]))
+    def __init__(self, learning_rate, discount_factor, eli_decay, epsilon=None):
+        self.epsilon = epsilon if epsilon else 0
+        self.policy_dict = defaultdict(lambda: 0)
+        self.eli_dict = defaultdict(lambda: 0)
+        self.discount_factor = discount_factor
+        self.eli_decay = eli_decay
+        self.learning_rate = learning_rate
 
-#main
+    # Updates policy using: current_policy + learning_rate*td_err*eli_dict[state,action]
+    def update_policy_dict(self, state, action, td_err):
+        self.policy_dict[(str(state), str(action))] += self.learning_rate * td_err * self.eli_dict[
+            (str(state), str(action))]
 
-#env = Environment()
-#a = Actor()
-#c = Critic()
+    # Updates eligibility using: discount_factor*eli_decay*eli_dict[state,action]
+    def update_eli_dict(self, state, action, i):
+        if i == 0:
+            self.eli_dict[(str(state), str(action))] = 1
+        else:
+            self.eli_dict[(str(state), str(action))] = self.discount_factor * self.eli_decay * self.eli_dict[
+                (str(state), str(action))]
 
-#for i in learning100:
+    def get_policy(self, state, action):
+        if (str(state), str(action)) in self.policy_dict.keys():
+            return self.policy_dict[(str(state), str(action))]
+        else:
+            return 0
+
+    def reset_eli_dict(self):
+        self.eli_dict = defaultdict(lambda: 0)
+
+    def get_action(self, state, legal_actions):
+        self.epsilon = self.epsilon*0.9999
+        if random.uniform(0, 1) >= self.epsilon:
+            return max(legal_actions, key=lambda action: self.get_policy(state, action))
+        return random.choice(legal_actions)
+
+
+# actor = Actor(0.2, 0.2, 0.2)
+#
+# actor.update_eli_dict([[1, 1, 1, 1], [1, 1, 1, 1], [1, 0, 1, 1], [1, 1, 1, 1]], [(2, 3), (2, 1)], 0)
+#
+# actor.update_policy_dict([[1, 1, 1, 1], [1, 1, 1, 1], [1, 0, 1, 1], [1, 1, 1, 1]], [(2, 3), (2, 1)], 0.3)
+# print(actor.policy_dict)
+# print(actor.get_policy([[1, 1, 1, 1], [1, 1, 1, 1], [1, 1, 1, 1], [1, 1, 1, 1]], [(2, 3), (2, 1)]))
+# print(actor.get_action([[1, 1, 1, 1], [1, 1, 1, 1], [1, 0, 1, 1], [1, 1, 1, 1]], [[(2, 1), (1, 1)], [(2, 3), (2, 1)]]))
+
+# main
+
+# env = Environment()
+# a = Actor()
+# c = Critic()
+
+# for i in learning100:
 #  env.newboardgame()
 #  current_state = env.getstate()
 #  legal_actions = env.get_legal_actions(current_state)
@@ -55,7 +64,7 @@ print(actor.get_action([[1, 1, 1, 1], [1, 1, 1, 1], [1, 0, 1, 1], [1, 1, 1, 1]],
 #  action = a.get_best_action(current_state, legal_actions)
 #  reward = env.perform(action)
 #  path.append(env.getstate())
-  
+
 #  td = critic.compute_td_err(newstate, reward)
 #  for i, state in enumerate(path):
 #    update_eli_dict(state, i)
--- main.py
+++ main.py
@@ -1,36 +1,52 @@
-from critic_dict import Critic_Dict
+from critic_dict import CriticDict
 from actor import Actor
 from simworld import Environment
 
+
 def main():
-  env = Environment(step_reward=1, final_reward=2, loser_penalty=-5, boardsize=4, open_cells=[(2,1)], board_type="Diamond", track_history=False)
-  critic = Critic_Dict(learning_rate=0.2, eli_decay=0.2, discount_factor=0.2)
-  actor = Actor(learning_rate=0.2, discount_factor=0.2, eli_decay=0.2, epsilon=0.1)
+    env = Environment(step_reward=10, final_reward=20, loser_penalty=-50, boardsize=4, open_cells=[(2, 1)],
+                      board_type="Diamond", track_history=True)
+    critic = CriticDict(learning_rate=0.1, eli_decay=0.7, discount_factor=0.7)
+    actor = Actor(learning_rate=0.1, discount_factor=0.7, eli_decay=0.7, epsilon=0.5)
 
-  for e in range(10):
+    for episode in range(1000):
+        env.new_game()
+        path = []
+        print(f"Playing episode number {episode+1}")
+        while not env.game_is_finished():
+            current_state = env.get_state()
+            legal_actions = env.get_actions()
+
+            action = actor.get_action(current_state, legal_actions)
+            reward = env.perform_action(action)
+            path.append(current_state)
+
+            td_err = critic.compute_td_err(current_state, action, reward)
+
+            for i, state in enumerate(path):
+                critic.update_eli_dict(state, td_err, i)
+                critic.update_value_dict(state, td_err)
+                actor.update_eli_dict(state, action, i)
+                actor.update_policy_dict(state, action, td_err)
+
+
+
+        critic.reset_eli_dict()
+        actor.reset_eli_dict()
+
+    env.board.show_gameplay()
+
     env.new_game()
-    path = []
+
+    print(f"Actor final epsilon: {actor.epsilon}")
+    actor.epsilon = 0
+    print("Attempting final gameplay to show you how smart I am now")
+    print(actor.policy_dict)
     while not env.game_is_finished():
-      current_state = env.get_state()
-      legal_actions = env.board.get_legal_actions()
-      print(current_state)
-      print(legal_actions)
-
-      action = actor.get_action(current_state, legal_actions)
-      print(action)
-      reward = env.perform_action(action)
-      print(reward)
-      path.append(env.get_state())
-      print(current_state.get_value())
-
-      td_err = critic.compute_td_err(current_state, action, reward)
-      for i, state in enumerate(path):
-        critic.update_value_dict(state,td_err)
-        critic.update_eli_dict(state, td_err, i)
-        actor.update_policy_dict(state, action, td_err)
-        actor.update_eli_dict(state, action,i)
-
-    critic.reset_eli_dict()
-    actor.reset_eli_dict()
+        current_state = env.get_state()
+        legal_actions = env.get_actions()
+        action = actor.get_action(current_state, legal_actions)
+        env.perform_action(action)
+    env.board.show_gameplay()
 
 main()
