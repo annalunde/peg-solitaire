2da5e08578c12f176331baed944f648db8ea9b4d
Emil Nylen-Forthun <emil.nylen.forthun@gmail.com>
2021-02-02 19:33:09 +0100

    Code cleanup and some more debugging

--- critic_dict.py
+++ critic_dict.py
@@ -11,33 +11,43 @@ class CriticDict:
         self.eli_decay = eli_decay
         self.discount_factor = discount_factor
 
-        # Computes TD error from formula: r + discount_factor*V(s') - V(s)
-
-    def compute_td_err(self, current_state, next_state,
-                       reward):  # Skal reward tas inn her, eller skal det tas inn ved initialisering?
+    def compute_td_err(self, current_state, next_state, reward):
+        """
+        Computes TD error from formula: r + discount_factor*V(s') - V(s)
+        """
         return reward + self.discount_factor * self.get_value(next_state) - self.get_value(current_state)
 
-    # Updates dictionary with the formula: V(S) = V(s) + learning_rate*td_err*eligibility
-    def update_value_dict(self, state, td_err):  # Oppdater for å ta inn SAP om nødvendig?
+    def update_value_dict(self, state, td_err):
+        """
+        Updates dictionary with the formula: V(S) = V(s) + learning_rate*td_err*eligibility
+        """
         if str(state) in self.value_dict.keys():
-          self.value_dict[str(state)] += self.learning_rate * td_err * self.eli_dict[str(state)]
+            self.value_dict[str(state)] += self.learning_rate * td_err * self.eli_dict[str(state)]
         else:
-          self.value_dict[str(state)] = self.learning_rate * td_err * self.eli_dict[str(state)]
+            self.value_dict[str(state)] = self.learning_rate * td_err * self.eli_dict[str(state)]
 
-    # Updates eligibility dictionary using replacing traces: 1 if S = St, discount_factor*eli_decay*eli_dict[state]
-    def update_eli_dict(self, state, td_err, i):  # i is index of state in history
+    def update_eli_dict(self, state, i):  # i is index of state in history
+        """
+        Updates eligibility dictionary using replacing traces: 1 if S = St (that is, i=0),
+        else decay by discount factor and eli_decay discount_factor*eli_decay*eli_dict[state]
+        """
         if i == 0:
             self.eli_dict[str(state)] = 1
         else:
             self.eli_dict[str(state)] = self.discount_factor * self.eli_decay * self.eli_dict[str(state)]
 
-    # Return value for given state
     def get_value(self, state):
+        """
+        Return value for given state
+        If state has not been seen before, return random initial value between 0 and 0.1
+        """
         if str(state) in self.value_dict.keys():
             return self.value_dict[str(state)]
         else:
-            return random.randint(0, 1)
+            return random.uniform(0, 0.1)
 
-    # Reset eli_dict after episode ends
     def reset_eli_dict(self):
-        self.eli_dict = {}
+        """
+        Reset eli_dict after episode ends
+        """
+        self.eli_dict = defaultdict(lambda: 0)
--- main.py
+++ main.py
@@ -1,52 +1,71 @@
 from critic_dict import CriticDict
 from actor import Actor
 from simworld import Environment
+from math import exp
 
 
-def main():
-    env = Environment(step_reward=10, final_reward=20, loser_penalty=-50, boardsize=4, open_cells=[(2, 1)],
-                      board_type="Diamond", track_history=True)
-    critic = CriticDict(learning_rate=0.1, eli_decay=0.5, discount_factor=0.7)
-    actor = Actor(learning_rate=0.1, discount_factor=0.5, eli_decay=0.7, epsilon=0.9)
-
-    for episode in range(10000):
-        env.new_game()
-        path = []
-        print(f"Playing episode number {episode+1}")
-        while not env.game_is_finished():
-            current_state = env.get_state()
-            legal_actions = env.get_actions()
-
-            action = actor.get_action(current_state, legal_actions)
-            reward = env.perform_action(action)
-            path.append(current_state)
-
-            td_err = critic.compute_td_err(current_state, action, reward)
-
-            for i, state in enumerate(path):
-                critic.update_eli_dict(state, td_err, i)
-                critic.update_value_dict(state, td_err)
-                actor.update_eli_dict(state, action, i)
-                actor.update_policy_dict(state, action, td_err)
-
-
-
-        critic.reset_eli_dict()
-        actor.reset_eli_dict()
-
-    env.board.show_gameplay()
-
-    env.new_game()
-
-    print(f"Actor final epsilon: {actor.epsilon}")
-    actor.epsilon = 0
-    print("Attempting final gameplay to show you how smart I am now")
-    #print(actor.policy_dict)
+def play(env, actor):
+    """
+    Plays a single game within the environment, used for playing final game after learning phase
+    """
     while not env.game_is_finished():
         current_state = env.get_state()
         legal_actions = env.get_actions()
         action = actor.get_action(current_state, legal_actions)
+        print(f"Value of state action pair \n{current_state} \n{action}: {actor.get_policy(current_state, action)}")
         env.perform_action(action)
-    env.board.show_gameplay()
+    return env
+
+
+def main():
+    wins = 0
+    num_episodes = 1
+    env = Environment(step_reward=1, final_reward=10, loser_penalty=-5, boardsize=4, open_cells=[(2, 1)],
+                      board_type="Diamond", track_history=False)
+    critic = CriticDict(learning_rate=0.5, eli_decay=0.9, discount_factor=0.9)
+    actor = Actor(learning_rate=0.5, discount_factor=0.9, eli_decay=0.9, epsilon=1, epsilon_decay=exp(-1 / num_episodes))
+
+    for episode in range(num_episodes):
+        env.new_game()
+        path = []
+        print(f"Playing episode number {episode + 1}")
+        while not env.game_is_finished():
+            current_state = env.get_state()
+            legal_actions = env.get_actions()
+            action = actor.get_action(current_state, legal_actions)
+            path.append((current_state, action))
+            reward = env.perform_action(action)
+
+            td_err = critic.compute_td_err(current_state, env.get_state(), reward)
+            for i, state_action_pair in enumerate(path[::-1]):
+                state, action = state_action_pair[0], state_action_pair[1]
+                critic.update_eli_dict(state, i)
+                critic.update_value_dict(state, td_err)
+                print(f"Actor eli dict: {actor.eli_dict}")
+                actor.update_eli_dict(state, action, i)
+                print(f"Actor eli dict: {actor.eli_dict}")
+                actor.update_policy_dict(state, action, td_err)
+            print()
+
+        #print("Value of first state-action pair:")
+        #print(actor.get_policy([[1, 1, 1, 0], [1, 1, 0, 1], [1, 1, 1, 1], [1, 1, 1, 1]], action=[(0, 3), (2, 1)]))
+
+        # Quit after first success
+        if env.board.is_won():
+            wins += 1
+
+        critic.reset_eli_dict()
+        actor.reset_eli_dict()
+
+    #env.board.show_gameplay()
+    print(f"Total number of wins: {wins}")
+    print(f"Actor final epsilon: {actor.epsilon}")
+    print("Attempting final gameplay to show you how smart I am now")
+
+    env.new_game()
+    actor.epsilon = 0
+    #env = play(env, actor)
+    #env.board.show_gameplay()
+
 
 main()
